db53e4fe5602be2d7df209f04b3ae4e919d0ec09
fadillzzz <fadillzzz@gmail.com>
2024-06-24 16:14:04 +0700

    Adds current user profile endpoints

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,16 @@
 import * as express from 'express';
 import {auth, requiresAuth} from 'express-openid-connect';
 import * as cors from 'cors';
+import * as bodyParser from 'body-parser';
+import {ManagementClient} from 'auth0';
 
 const env = require(process.cwd() + '/env.json');
+
+const auth0Management = new ManagementClient({
+  domain: new URL(env.issuerBaseURL).hostname,
+  clientId: env.clientID,
+  clientSecret: env.clientSecret,
+});
 
 const app = express();
 const config = {
@@ -14,6 +22,8 @@ const config = {
   issuerBaseURL: env.issuerBaseURL,
 };
 
+app.use(bodyParser.json());
+
 app.use(
   cors({
     origin: env.frontendURL,
@@ -21,25 +31,50 @@ app.use(
   })
 );
 
-app.use(auth({...config, routes: {login: false, logout: false}}));
+app.use(
+  auth({
+    ...config,
+    routes: {login: false, logout: false},
+  })
+);
 
 app.get('/login', (req, res) => {
-  res.oidc.login({returnTo: env.frontendURL + '?loggedIn=true'});
+  res.oidc.login({
+    returnTo: env.frontendURL + '?loggedIn=true',
+  });
 });
 
-app.get('/logout', (req, res) => {
+app.get('/logout', requiresAuth(), (req, res) => {
   res.oidc.logout({returnTo: env.frontendURL});
 });
 
-app.get('/authenticated', (req, res) => {
-  res.send({loggedIn: req.oidc.isAuthenticated()});
+app.get('/profile', requiresAuth(), async (req, res) => {
+  // Retrieve user profile from the API instead of relying on the
+  // OIDC object since that information doesn't get updated in real-time (requires reauthentication)
+  const response = await auth0Management.users.get({id: req.oidc.user!.sub});
+  const user = {
+    name: response.data.name,
+    email: response.data.email,
+  };
+
+  res.send(user);
 });
 
-app.get('/profile', requiresAuth(), (req, res) => {
-  res.send({
-    email: req.oidc.user!.email,
-    name: req.oidc.user!.name,
-  });
+app.patch('/profile', requiresAuth(), async (req, res) => {
+  const {name} = req.body;
+
+  if (!name) {
+    res.status(400).send({error: 'Name is required'});
+    return;
+  }
+
+  try {
+    await auth0Management.users.update({id: req.oidc.user!.sub}, {name});
+    res.status(204).send();
+  } catch (e) {
+    console.error(e);
+    res.status(500).send({error: 'Failed to update profile'});
+  }
 });
 
 app.listen(8000, '0.0.0.0');
